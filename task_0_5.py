74f8ed52a060b33a6d8fe38c4596104a81c4fb8e
Collin Makwala <collinmakwala@gmail.com>
2022-05-23 20:45:46 +0200

    corrections to task_0_5 and task_0_6

--- task_0_5.py
+++ task_0_5.py
@@ -1,6 +1,10 @@
+from math import sqrt
+
+
 def area_of_triangle(side_a, side_b, side_c):
-    area = 1 / 2 * (side_a + side_b + side_c)
+    semi = 1 / 2 * (side_a + side_b + side_c)
+    area = sqrt(semi * (semi - side_a) * (semi - side_b) * (semi - side_c))
     return area
 
 
-print(area_of_triangle(6, 8, 4))
+print(area_of_triangle(3, 4, 5))
--- task_0_6.py
+++ task_0_6.py
@@ -1,16 +1,4 @@
-def maximum(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2 >= num3:
-        return num2
-    else:
-        return num3
-
-
-print(maximum(1, 2, 3))
-
-# BONUS
-def maximum2(*numbers):
+def maximum(*numbers):
     max_num = 0
     for number in numbers:
         if number > max_num:
@@ -18,4 +6,4 @@ def maximum2(*numbers):
     return max_num
 
 
-print(maximum2(1, 22, 3, 2))
+print(maximum(1, 22, 3, 2))
