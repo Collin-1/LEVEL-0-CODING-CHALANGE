7527bb672b204e2339d62d7828de3dcc30ac8385
Collin Makwala <collinmakwala@gmail.com>
2022-05-24 18:25:41 +0200

    changed function in task_0_6 to handle both negative and positive numbers

--- task_0_6.py
+++ task_0_6.py
@@ -1,9 +1,9 @@
 def maximum(*numbers):
-    max_num = 0
+    max_num = numbers[0]
     for number in numbers:
         if number > max_num:
             max_num = number
     return max_num
 
 
-print(maximum(1, 22, 3, 2))
+print(maximum(-1, -22, -3, -2))
